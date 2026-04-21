09ebd87cb0f15d86c8fcf38ff7fe9c3e3730078e
cdwolfling <cdwolfling@gmail.com>
2026-04-21 15:21:05 +0800

    update [spec].[Parameter_Xlsx_Yaml_Map_ProductModel]

--- ProDB/spec/Tables/Parameter_Xlsx_Yaml_Map_ProductModel.sql
+++ ProDB/spec/Tables/Parameter_Xlsx_Yaml_Map_ProductModel.sql
@@ -1,13 +1,13 @@
 CREATE TABLE [spec].[Parameter_Xlsx_Yaml_Map_ProductModel] (
-    [ParameterId]   INT          NOT NULL,
+    [MapId]   INT          NOT NULL,
     [ProductModel]  VARCHAR(20)  NOT NULL,
     [Data_column]   VARCHAR(100) NOT NULL,
     [CreatedOn]     DATETIME     NOT NULL CONSTRAINT [DF_spec_PXYM_PM_CreatedOn] DEFAULT (GETDATE()),
     [CreatedBy]     NVARCHAR(50) NULL,
     CONSTRAINT [PK_spec_PXYM_ProductModel]
-        PRIMARY KEY CLUSTERED ([ParameterId], [ProductModel]),
-    CONSTRAINT [FK_spec_PXYM_ProductModel_ParameterDef]
-        FOREIGN KEY ([ParameterId]) REFERENCES [spec].[ParameterDef]([ParameterId]),
+        PRIMARY KEY CLUSTERED ([MapId], [ProductModel]),
+    CONSTRAINT [FK_spec_PXYM_ProductModel_ParameterMap]
+        FOREIGN KEY ([MapId]) REFERENCES [spec].[Parameter_Xlsx_Yaml_Map]([ID]),
     CONSTRAINT [FK_spec_PXYM_ProductModel_ProductModel]
         FOREIGN KEY ([ProductModel]) REFERENCES [dbo].[ProductModel]([ProductModel])
 );
